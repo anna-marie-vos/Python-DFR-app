9b308d57f5f90e7c2ae521d4f5a78b0df985bc04
anna-marie-vos <amvos@outlook.com>
2017-06-11 13:03:20 +1200

    dummy object added

--- UI/index.py
+++ UI/index.py
@@ -1,7 +1,11 @@
 from tkinter import *
 from UI.DFRCreator import DFR
+from db.state import Service
 
-dfr = DFR()
+Dfr = DFR()
+service = Service('service')
+
+print(service.service)
 
 class Window():
     def __init__(self, window):
@@ -10,6 +14,16 @@ class Window():
 
         title1 = Label(window, text = "DFR Template Creator")
         title1.grid(row = 0, column = 0, columnspan = 11 )
+
+        title2 = Label(window, text="Service")
+        title2.grid(row = 1, column = 0, columnspan = 6 )
+
+        title3 = Label(window, text="Components")
+        title3.grid(row = 1, column = 7, columnspan = 6 )
+
+        checked = Variable()
+        item = Checkbutton(window, text="item1", variable=checked)
+        item.grid(row=2, column = 0, columnspan = 3)
 
 window=Tk()
 Window(window)
---
+++ db/state.py
@@ -0,0 +1,20 @@
+
+
+class Service(object):
+    service = {}
+
+
+    def __init__(self,service):
+        """arguments:service, heading2, heading3, paragraphs """
+        self.service = service
+        self.service = {
+            "1": "Mechanical",
+            "1.1": {
+                "heading" : "FCUs",
+                "para": "blblbl blbl blblb blblb"
+            },
+            "1.2": {
+                "heading": "Chilled Beams",
+                "para": "blab blab blab blab"
+            }
+        }
