9d4dd8f38b0f4a247bfe64ab9843f90e2847e8c9
anna-marie-vos <amvos@outlook.com>
2017-06-17 14:14:50 +1200

    Ui and connected with index

---
+++ UI/Func.py
@@ -0,0 +1,6 @@
+import sys
+from tkinter import *
+import UI.index
+
+def say():
+    say = Label(text="howdy").pack()
--- UI/index.py
+++ UI/index.py
@@ -1,15 +1,24 @@
 import sys
 from tkinter import *
+from UI.Func import *
 
 window = Tk()
 
-window.title("DFR creator") #this is the title / can also use wm_title()
-window.geometry("500x500+100+100") #"XDimxYDim+screenDiff+screenDiff"
+window.title("DFR creator")         #this is the title / can also use wm_title()
+window.geometry("500x500+100+100")  #"XDimxYDim+screenDiff+screenDiff"
 
-#fg = foreground, bg=background
-TemplateLabel = Label(text="Template Creator",
+                                    #fg = foreground, bg=background
+TemplateLabel = Label(window,text="TEMPLATE CREATOR",
     fg="#009900", bg="#d9ffb3", relief="raised")
-TemplateLabel.pack()
-TemplateLabel.place(x="10",y="10")
+TemplateLabel.pack()                #places things in the centre
+TemplateLabel.place(x="5",y="5")    #if you dont use a grid
+                                    # TemplateLabel.grid(row="0", column="0")
+                                    # sticky = align text W(west), E(east), N(north), S(south)
+
+createWordButton = Button(window, text="CONVERT TO WORD",
+    command=say)
+createWordButton.pack()
+
+
 
 window.mainloop() #loop
