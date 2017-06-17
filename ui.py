3441a84644278b00a13071a07c2e8b7282d2e038
anna-marie-vos <amvos@outlook.com>
2017-06-17 17:25:50 +1200

    tickboxes works and records 1 or 0

--- ui.py
+++ ui.py
@@ -1,54 +1,57 @@
 import sys
 from tkinter import *
 
-data = {
-    'Mechanical':{
-        '1.1':'Fan Coil Units',
-        'paragraphs':['para para prar',
-            'parap',
-            'para 2 thr thrthrth hththr'
-        ]
-    },
-    'Electrical':{
-        '1.1':'Conduit',
-        'paragraphs':['elec elec elec',
-            'elec2 ele2 ele2',
-            'ele3 ele3 elec3'
-        ]
-    },
-        'Hydraulics':{
-            '1.1':'pipes',
-            'paragraphs':['pipe pipe pipe',
-                'pipe2 pipe2 pipe2',
-                'pipe3 pipe3 pipe3'
-            ]
-        },
-}
+class Window(object):
+    def __init__(self,window):
+        self.Data()
 
-window = Tk()
-window.geometry("500x500+100+100")
-window.title("DFR Template Maker")
+        self.window = window
+        self.window.geometry("500x500+100+100")
+        self.window.wm_title("DFR Creator")
 
-title1 = Label(window,text = "DFR Template maker")
-title1.grid(row="0",column="0")
+        self.title1 = Label(window,text = "DFR Template maker")
+        self.title1.grid(row="0",column="0")
 
-serviceLabel = Label(window, text="Services:")
-serviceLabel.grid(row="1", column="0")
+        serviceLabel = Label(window, text="Services:")
+        serviceLabel.grid(row="1", column="0")
 
-# var = IntVar()
-# c = Checkbutton(window, text="Expand", variable=var)
-# c.grid(row=1,column=1)
+        self.renderChecks()
 
-def checkboxes():
-    i=0
-    for k, v in data.items():
-        i += 1
-        print (k, ", ")
-        v = IntVar()
-        c = Checkbutton(window, text=k, variable=v)
-        c.grid(row=i,column=1)
-        c.var = v
-        print(c.var.get())
-checkboxes()
+    def Data(self):
+        self.data = {
+        'Mechanical':{
+            '1.1':'Fan Coil Units',
+            'paragraphs':[
+                'mech1 mech1 mech1',
+                'mech2',
+                'mech3 mech3 mech3 mech3 mech3',
+                ]
+            },
+        'Electrical':{
+            '1.1':'conduits',
+            'paragraphs':[
+                'elec elec elec',
+                'elec1',
+                'elec2 elec2 elec2 elec2',
+                ]
+            },
+        }
 
+    def renderChecks(self):
+        i = 0
+        servicesData = self.data
+        self.var = {}
+        for key, value in servicesData.items():
+            i +=1
+            self.var.update({key:IntVar()})
+            service = Checkbutton(window, text=key,
+                variable=self.var[key], command=self.recordTick)
+            service.grid(row = i, column = 1)
+
+    def recordTick(self):
+        print('electrical',self.var['Electrical'].get())
+        print('Mechanical',self.var['Mechanical'].get())
+
+window=Tk()
+Window(window)
 window.mainloop()
